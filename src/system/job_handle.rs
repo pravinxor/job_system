a5f11633bc53dcac6715ed4505b734ec12414e31
Pravin Ramana <pravinramana25@protonmail.ch>
2023-10-15 18:01:45 -0500

    Update job handle with get function

--- src/system/job_handle.rs
+++ src/system/job_handle.rs
@@ -2,14 +2,27 @@ use std::sync::{Arc, Condvar, Mutex};
 
 use serde::Serialize;
 
+pub(crate) struct HandleInner<T: Serialize> {
+    data: Mutex<Option<T>>,
+    available: Condvar,
+}
+
 /// A handle that is returned after the system takes a job
 pub struct JobHandle<T: Serialize> {
-    result: Arc<(Mutex<Option<T>>, Condvar)>,
+    result: Arc<HandleInner<T>>,
 }
 
 impl<T: Serialize> JobHandle<T> {
-    /// Blocks the current thread until the result is available
+    /// Consumes the JobHandle and blocks the current thread until the result is available
     pub fn get(self) -> T {
-        todo!()
+        let mut data_guard = self.result.data.lock().unwrap();
+        // Similar to the message_queue, loop until the data is Some, because the condition variable may spuriously wake up
+        loop {
+            if let Some(data) = data_guard.take() {
+                return data;
+            } else {
+                data_guard = self.result.available.wait(data_guard).unwrap();
+            }
+        }
     }
 }

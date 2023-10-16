6d60808fcf03fb58695696101e1fe44c2f4957ad
Pravin Ramana <pravinramana25@protonmail.ch>
2023-10-16 09:21:58 -0500

    Create jobsystem

--- src/system/job_system.rs
+++ src/system/job_system.rs
@@ -1,1 +1,34 @@
-pub struct JobSystem {}
+use std::sync::Arc;
+
+use serde::Serialize;
+
+use super::{
+    job_handle::JobHandle,
+    message_queue::MessageQueue,
+    worker::{Worker, WorkerMessage},
+};
+
+pub(crate) struct JobSystem<T: Serialize + Send + Sync + 'static> {
+    workers: Vec<Worker>,
+    message_queue: Arc<MessageQueue<WorkerMessage<T>>>,
+}
+
+impl<T: Serialize + Send + Sync + 'static> JobSystem<T> {
+    pub(crate) fn new() -> Self {
+        Self {
+            message_queue: MessageQueue::new(),
+            workers: Vec::new(),
+        }
+    }
+
+    pub(crate) fn add_worker(&mut self) {
+        self.workers.push(Worker::new(self.message_queue.clone()));
+    }
+
+    pub(crate) fn send_job(&mut self, x: T, f: fn(T) -> T) -> JobHandle<T> {
+        let handle = JobHandle::new(x, f);
+        self.message_queue
+            .send(WorkerMessage::Handle(handle.handle_inner.clone()));
+        handle
+    }
+}

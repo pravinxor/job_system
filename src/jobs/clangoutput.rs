90c4669608d8408d71cbfd0b7289cb9712e5723d
Pravin Ramana <pravinramana25@protonmail.ch>
2023-10-17 11:19:00 -0500

    Change json schema

--- src/jobs/clangoutput.rs
+++ src/jobs/clangoutput.rs
@@ -14,22 +14,35 @@ lazy_static! {
 
 pub fn parse(input: Value) -> Value {
     if let Some(clang_output) = input["clang_output"].as_str() {
-        let mut output = json!({"files" : {}, "linker": {"message" : "", "symbols": []}});
+        let mut output = json!({"files" : [], "linker": {"message" : "", "symbols": []}});
         for line in clang_output.lines() {
             if let Some(caps) = COMPILER_EXPR.captures(line) {
-                let file_entry = output["files"]
-                    .as_object_mut()
-                    .unwrap()
-                    .entry(&caps["filename"])
-                    .or_insert_with(|| json!([]))
-                    .as_array_mut()
-                    .unwrap();
-
-                file_entry.push(json!({
+                let filename = &caps["filename"];
+                let error_entry = json!({
                     "line": &caps["line"],
                     "column": &caps["column"],
                     "message": &caps["message"]
-                }));
+                });
+
+                let file_entry = match output["files"]
+                    .as_array_mut()
+                    .unwrap()
+                    .iter_mut()
+                    .find(|file| file["filename"] == filename)
+                {
+                    Some(file_entry) => file_entry,
+                    None => {
+                        let file_entry = json!({"filename" : filename, "errors": []});
+                        let files = output["files"].as_array_mut().unwrap();
+                        files.push(file_entry);
+                        files.last_mut().unwrap()
+                    }
+                };
+
+                file_entry["errors"]
+                    .as_array_mut()
+                    .unwrap()
+                    .push(error_entry);
             } else if let Some(caps) = LINKER_EXPR.captures(line) {
                 output["linker"]["message"] = json!({"message": &caps["message"]});
             } else if LINKER_TXT_EXPR.is_match(line) {

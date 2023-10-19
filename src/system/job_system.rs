5c1048fedb52db90d2471521757b48b73a9ac024
Pravin Ramana <pravinramana25@protonmail.ch>
2023-10-19 16:03:20 -0500

    Refactor ffi implementation

--- src/system/job_system.rs
+++ src/system/job_system.rs
@@ -57,8 +57,8 @@ pub mod ffi {
     use super::JobSystem;
 
     lazy_static! {
-        static ref JOB_MAP: DashMap<u64, JobHandle<Value>> = DashMap::new();
         static ref ID_COUNTER: AtomicU64 = AtomicU64::new(0);
+        static ref JOB_MAP: DashMap<u64, JobHandle<Value>> = DashMap::new();
         static ref SYSTEM_MAP: DashMap<u64, Mutex<JobSystem<Value>>> = DashMap::new();
     }
 
@@ -73,82 +73,100 @@ pub mod ffi {
         }
     }
 
+    macro_rules! parse_json_from_str {
+        ($input_str:expr) => {{
+            Value::from_str($input_str).map_err(|_| "Unable to parse json")
+        }};
+    }
+    macro_rules! fetch_system_from_json {
+        ($job_json:expr) => {{
+            let system_id = $job_json["system_id"]
+                .as_u64()
+                .ok_or("'system_id' key is not a valid number or may not exist")?;
+
+            let system = SYSTEM_MAP
+                .get_mut(&system_id)
+                .ok_or("Specified system id could not be found");
+
+            system
+        }};
+    }
+
     #[no_mangle]
-    pub extern "C" fn create_jobsystem(json_str_ptr: *const c_char) -> *const c_char {
+    pub extern "C" fn create_jobsystem() -> *const c_char {
         let system = Mutex::new(JobSystem::new());
         let id = ID_COUNTER.fetch_add(1, Relaxed);
         SYSTEM_MAP.insert(id, system);
         let output_json = json!({"success" : true, "system_id" : id});
 
         let c_str = CString::new(output_json.to_string()).unwrap();
-        return c_str.into_raw();
+        c_str.into_raw()
     }
 
     #[no_mangle]
     pub extern "C" fn get_job(json_str_ptr: *const c_char) -> *const c_char {
         assert!(!json_str_ptr.is_null());
-        let input_str = unsafe { CStr::from_ptr(json_str_ptr).to_str().unwrap() };
-
-        let output_json;
-        if let Ok(job_json) = Value::from_str(input_str) {
-            if let Some(handle_id) = job_json["handle_id"].as_u64() {
-                if let Some(handle) = JOB_MAP.remove(&handle_id).and_then(|e| Some(e.1)) {
-                    let result = handle.get();
-                    output_json = json!({"success" : true, "result" : result});
-                } else {
-                    output_json =
-                        json!({"success" : false, "error" : "specified handle id was not found"});
-                }
-            } else {
-                output_json = json!({"success": false,"error" : "'type' handle_id is not an int or may not exist"});
-            }
+        let output_json = if json_str_ptr.is_null() {
+            json!({"error" : "json_str_ptr was a null pointer"})
         } else {
-            output_json = json!({"success" : false, "error" : "unable to parse input, job_json"})
-        }
+            let input_str = unsafe { CStr::from_ptr(json_str_ptr).to_str().unwrap() };
 
+            match process_and_query_job(input_str) {
+                Ok(handle_id) => json!({"success" : true, "handle_id" : handle_id}),
+                Err(message) => json!({"success" : false, "error" : message}),
+            }
+        };
         let c_str = CString::new(output_json.to_string()).unwrap();
 
         c_str.into_raw()
     }
 
+    fn process_and_query_job(input_str: &str) -> Result<Value, String> {
+        let job_json = parse_json_from_str!(input_str)?;
+
+        let handle_id = job_json["handle_id"]
+            .as_u64()
+            .ok_or("'type' handle_id is not a valid number or may not exist")?;
+
+        let handle = JOB_MAP
+            .remove(&handle_id)
+            .map(|e| e.1)
+            .ok_or("specified handle id was not found")?;
+
+        Ok(handle.get())
+    }
+
     #[no_mangle]
     /// Sends the specified command to the JobSystem, given a JSON with key "type", specifying jobtype and "input", specifying the input data for the job.
     pub extern "C" fn send_job(json_str_ptr: *const c_char) -> *const c_char {
-        let output_json;
-        if json_str_ptr.is_null() {
-            output_json = json!({"error" : "json_str_ptr was a null pointer"});
+        let output_json = if json_str_ptr.is_null() {
+            json!({"error" : "json_str_ptr was a null pointer"})
         } else {
             let input_str = unsafe { CStr::from_ptr(json_str_ptr).to_str().unwrap() };
 
-            output_json = match process_and_load_job(input_str) {
+            match process_and_load_job(input_str) {
                 Ok(handle_id) => json!({"success" : true, "handle_id" : handle_id}),
                 Err(message) => json!({"success" : false, "error" : message}),
-            };
-        }
+            }
+        };
 
         CString::new(output_json.to_string()).unwrap().into_raw()
     }
 
     fn process_and_load_job(input_str: &str) -> Result<u64, String> {
-        let job_json = Value::from_str(input_str).map_err(|_| "unable to parse input, job_json")?;
+        let job_json = parse_json_from_str!(input_str)?;
 
-        let system_id = job_json["system_id"]
-            .as_u64()
-            .ok_or_else(|| "'type' system_id is not a valid number or may not exist")?;
-
-        let system = SYSTEM_MAP
-            .get_mut(&system_id)
-            .ok_or_else(|| "Specified system id could not be found")?;
+        let system = fetch_system_from_json!(job_json)?;
 
         let job_type = job_json["type"]
             .as_str()
-            .ok_or_else(|| "'type' key is not a string or may not exist")?;
+            .ok_or("'type' key is not a string or may not exist")?;
 
         let job: Option<fn(Value) -> Value> = map_job_identifier(job_type);
 
-        let job_fn = job.ok_or_else(|| format!("job type '{}' was not found", job_type))?;
+        let job_fn = job.ok_or(format!("job type '{}' was not found", job_type))?;
 
-        let id = ID_COUNTER.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+        let id = ID_COUNTER.fetch_add(1, Relaxed);
         let input = job_json["input"].clone();
         let mut system = system.lock().unwrap();
         let handle = system.send_job(input, job_fn);

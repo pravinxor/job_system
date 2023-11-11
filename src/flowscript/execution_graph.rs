eabc5037d14aac1bea900763095865a1ea54c663
Pravin Ramana <pravinramana25@protonmail.ch>
2023-11-11 14:26:16 -0600

    Implement parser for buillding execution graph

--- src/flowscript/execution_graph.rs
+++ src/flowscript/execution_graph.rs
@@ -1,65 +1,104 @@
 extern crate petgraph;
 use petgraph::graph::{DiGraph, NodeIndex};
-use std::collections::HashMap;
+use std::{collections::HashMap, iter::Peekable};
 
-use super::tokenizer::{BrState, Token, Tokenizer};
+use super::{
+    tokenizer::{BrState, Key, Token},
+    util::SpliteratorAdapter,
+};
 
 #[derive(Debug)]
 struct ProcessNode {
     name: String,
+    attributes: HashMap<Key, String>,
 }
 
 #[derive(Debug)]
 pub(crate) struct ExecutionGraph {
+    name: Option<String>,
     graph: DiGraph<ProcessNode, ()>, // Empty tuple `()` as edge weight
     node_indices: HashMap<String, NodeIndex>, // For quick node lookup
 }
 
 impl ExecutionGraph {
-    pub fn new() -> Self {
+    pub fn new(name: Option<String>) -> Self {
         ExecutionGraph {
+            name,
             graph: DiGraph::new(),
             node_indices: HashMap::new(),
         }
     }
 
-    fn add_edge(&mut self, tokens: &mut Vec<Token>) {
-        dbg!(&tokens);
-        tokens.clear();
-    }
+    fn parse_node_attributes(
+        &mut self,
+        node_name: &str,
+        attrs_tokens: &[Token],
+    ) -> Result<(), String> {
+        let node_index = self.get_or_create_node(node_name);
 
-    fn extract_digraph<I>(&mut self, tokens: &mut I) -> Result<(), String>
-    where
-        I: Iterator<Item = Token>,
-    {
-        let mut open_braces = 0;
-        let mut token_line = Vec::new();
-        while let Some(token) = tokens.next() {
-            match token {
-                Token::Brace(state) => {
-                    open_braces += match state {
-                        BrState::Open => 1,
-                        BrState::Closed => -1,
-                    }
+        let mut attributes = HashMap::new();
+        let mut i = 0;
+        while i < attrs_tokens.len() {
+            match (
+                attrs_tokens.get(i).take(),
+                attrs_tokens.get(i + 1),
+                attrs_tokens.get(i + 2).take(),
+            ) {
+                (Some(Token::ReservedText(key)), Some(Token::Equals), Some(Token::Text(value))) => {
+                    attributes.insert(*key, value.to_owned());
+                    i += 3;
                 }
-                Token::Arrow | Token::Text(_) | Token::Bracket(_) => token_line.push(token),
-                Token::Digraph => return Err(format!("Unexpected token: {:?}", token)),
-                Token::Semicolon => self.add_edge(&mut token_line),
+                _ => return Err(format!("Invalid attribute format in {:?}", attrs_tokens)),
             }
         }
+
+        if let Some(node) = self.graph.node_weight_mut(node_index) {
+            node.attributes = attributes;
+        }
+
         Ok(())
     }
 
-    pub fn from_tokens<I>(mut tokens: I) -> Result<Self, String>
+    fn parse_line(&mut self, tokens: Vec<Token>) -> Result<(), String> {
+        if tokens.is_empty() {
+            return Ok(());
+        }
+
+        match tokens.as_slice() {
+            [Token::Text(node_name), Token::Bracket(BrState::Open), ..] => {
+                // Handling node attributes
+                self.parse_node_attributes(node_name, &tokens[2..tokens.len() - 1])
+            }
+            [Token::Text(src_name), Token::Arrow, Token::Text(dest_name)] => {
+                // Handling directed edges
+                self.add_path(src_name, dest_name);
+                Ok(())
+            }
+            [Token::Brace(BrState::Closed)] => Ok(()),
+            _ => Err(format!("Unexpected token sequence: {:?}", tokens)),
+        }
+    }
+
+    pub fn from_tokens<I>(tokens: &mut Peekable<I>) -> Result<Self, String>
     where
         I: Iterator<Item = Token>,
     {
-        let mut graph = Self::new();
-        while let Some(token) = tokens.next() {
-            if token == Token::Digraph {
-                graph.extract_digraph(&mut tokens)?;
-            }
+        tokens
+            .next()
+            .ok_or("Expect digraph token at beginning of parse")?;
+        let graph_name;
+        match tokens.next().ok_or("Expected token after digraph")? {
+            Token::Brace(BrState::Open) => graph_name = None,
+            Token::Text(name) => graph_name = Some(name),
+            t => return Err(format!("Unexpected token after digraph: {:?}", t)),
         }
+        let mut graph = Self::new(graph_name);
+
+        let statement_lines = tokens.split_by(|t| *t == Token::Semicolon);
+        for line in statement_lines {
+            graph.parse_line(line)?;
+        }
+
         Ok(graph)
     }
 
@@ -79,12 +118,11 @@ impl ExecutionGraph {
             None => {
                 let node_index = self.graph.add_node(ProcessNode {
                     name: name.to_owned(),
+                    attributes: HashMap::new(),
                 });
                 self.node_indices.insert(name.to_owned(), node_index);
                 node_index
             }
         }
     }
-
-    // Additional methods like graph traversal, execution logic, etc.
 }

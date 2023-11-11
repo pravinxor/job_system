c0de4e597f59459a7b8f50ced8262816a4deb142
Pravin Ramana <pravinramana25@protonmail.ch>
2023-11-11 14:26:38 -0600

    Update main with example code

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,30 @@ mod flowscript;
 mod jobs;
 mod system;
 
+use flowscript::tokenizer::Token;
 use jobs::filereader;
 use serde_json::json;
 
 use crate::{
+    flowscript::execution_graph::ExecutionGraph,
+    flowscript::tokenizer::TokenizerAdapter,
+    flowscript::util::SpliteratorAdapter,
     jobs::{clangoutput, make},
     system::job_system::JobSystem,
 };
-use std::error::Error;
+use std::{
+    error::Error,
+    io::{self, Read},
+};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let tokens = flowscript::tokenizer::Tokenizer::new(
-        r#"
-            digraph {
-                a->b;
-                b -> c;
-            }
-            "#,
-    );
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input)?;
 
-    for token in tokens {
-        dbg!(token);
-    }
+    let tokens = input.into_bytes().into_iter().map(|b| b as char).tokens();
+
+    let graph = ExecutionGraph::from_tokens(&mut tokens.peekable())?;
+
+    dbg!(graph);
     Ok(())
 }

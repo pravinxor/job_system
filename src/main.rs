7757a867187005b78e94ac8480f5372b220e3ba8
Pravin Ramana <pravinramana25@protonmail.ch>
2023-11-13 14:49:59 -0600

    Update main to support parallel job execution graph build

--- src/main.rs
+++ src/main.rs
@@ -2,33 +2,76 @@ mod flowscript;
 mod jobs;
 mod system;
 
-use flowscript::tokenizer::Token;
-use jobs::filereader;
-use serde_json::json;
+use clap::Parser;
 
 use crate::{
-    flowscript::execution_graph::ExecutionGraph,
-    flowscript::tokenizer::TokenizerAdapter,
-    flowscript::util::SpliteratorAdapter,
-    jobs::{clangoutput, make},
+    flowscript::execution_graph::ExecutionGraph, flowscript::tokenizer::TokenizerAdapter,
     system::job_system::JobSystem,
 };
 use std::{
     error::Error,
+    fmt::Display,
+    fs::File,
     io::{self, Read},
 };
 
+#[derive(Parser)]
+#[clap(version = "1.0", author = "Pravin Ramana")]
+struct Args {
+    files: Vec<String>,
+}
+
+fn print_if_err<T, E>(r: Result<T, E>) -> Result<T, E>
+where
+    E: Sized + Display,
+{
+    if let Err(ref e) = r {
+        eprintln!("{}", e)
+    }
+    r
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut input = String::new();
-    io::stdin().read_to_string(&mut input)?;
+    let args = Args::parse();
 
-    let tokens = input.into_bytes().into_iter().map(|b| b as char).tokens();
+    let mut parser_system = JobSystem::new();
+    (0..args.files.len()).for_each(|_| parser_system.add_worker());
 
-    let tokens: Vec<_> = tokens.collect();
-    let mut tokens = tokens.into_iter().peekable();
+    let code_files = args
+        .files
+        .into_iter()
+        .map(|name| File::open(name))
+        .flat_map(|r| print_if_err(r))
+        .map(|f| -> io::Result<String> {
+            let mut f = f;
+            let mut data = String::new();
+            f.read_to_string(&mut data)?;
+            Ok(data)
+        })
+        .flat_map(|r| print_if_err(r));
 
-    let mut graph = ExecutionGraph::from_tokens(&mut tokens)?;
+    let graph_handles: Vec<_> = code_files
+        .into_iter()
+        .map(|code| {
+            parser_system.send_job(code, |code| {
+                let mut tokens = code
+                    .into_bytes()
+                    .into_iter()
+                    .map(|b| b as char)
+                    .tokens()
+                    .peekable();
+                ExecutionGraph::from_tokens(&mut tokens)
+            })
+        })
+        .collect();
 
-    let res = graph.execute_all();
+    let mut merged_graph: ExecutionGraph = graph_handles
+        .into_iter()
+        .map(|h| h.get())
+        .flat_map(|r| print_if_err(r))
+        .sum();
+
+    let _res = merged_graph.execute_all();
+
     Ok(())
 }

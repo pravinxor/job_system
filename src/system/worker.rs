026e1c7d623374c073aac04f46d3179ae2d62c21
Pravin Ramana <pravinramana25@protonmail.ch>
2023-11-12 20:19:49 -0600

    Update generics for jobsystem to allow different types to ingress and egress

--- src/system/worker.rs
+++ src/system/worker.rs
@@ -4,29 +4,36 @@ use super::{
     job_handle::{HandleInner, Status},
     message_queue::MessageQueue,
 };
-use serde::Serialize;
 
-pub(crate) enum WorkerMessage<T: Serialize> {
-    Handle(Arc<HandleInner<T>>),
+#[derive(Debug)]
+pub(crate) enum WorkerMessage<X, Y>
+where
+    X: Send + Sync,
+    Y: Send + Sync,
+{
+    Handle(Arc<HandleInner<X, Y>>),
 
     /// Notifies the thread to stop accepting jobs and exit its worker loop
     Join,
 }
 
+#[derive(Debug)]
 pub(crate) struct Worker {
     handle: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
-    pub(crate) fn new<T: Serialize + Send + Sync + 'static>(
-        message_receiver: Arc<MessageQueue<WorkerMessage<T>>>,
+    pub(crate) fn new<X: Send + Sync + 'static, Y: Send + Sync + 'static>(
+        message_receiver: Arc<MessageQueue<WorkerMessage<X, Y>>>,
     ) -> Self {
         Self {
             handle: Some(thread::spawn(|| Self::worker_loop(message_receiver))),
         }
     }
 
-    fn worker_loop<T: Serialize>(message_receiver: Arc<MessageQueue<WorkerMessage<T>>>) {
+    fn worker_loop<X: Send + Sync, Y: Send + Sync>(
+        message_receiver: Arc<MessageQueue<WorkerMessage<X, Y>>>,
+    ) {
         while let WorkerMessage::Handle(handle) = message_receiver.recv() {
             if let Some(x) = handle.x.lock().unwrap().take() {
                 let func = handle.f;

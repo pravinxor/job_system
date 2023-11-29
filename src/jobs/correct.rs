cdc52d2fbee686779dfafefad669d9e2b45187fd
Pravin Ramana <pravinramana25@protonmail.ch>
2023-11-28 19:49:11 -0600

    Update correct.rs to support showing linker errors

--- src/jobs/correct.rs
+++ src/jobs/correct.rs
@@ -2,12 +2,47 @@ use openai_api_rust::{chat::*, *};
 use serde_json::{json, Value};
 use std::error::Error;
 
-pub fn error_fix(llm: &OpenAI, error: &Value) -> Result<Value, Box<dyn Error>> {
+fn fix_linker_err(llm: &OpenAI, error: &Value) -> Result<Value, Box<dyn Error>> {
+    let linker_msg = error["message"].as_str().ok_or("message not found")?;
+
+    if linker_msg.is_empty() {
+        return Ok(json!(null));
+    }
+
+    let sym_messages = error["symbols"]
+        .as_array()
+        .ok_or("symbols not found")?
+        .iter()
+        .flat_map(|sym| sym["message"].as_str())
+        .fold(String::new(), |mut acc, line| {
+            acc.push_str(line);
+            acc.push('\n');
+            acc
+        });
+    let post_prompt = r#"A fully JSON response with the schema: {"message": string, "fix": string} and no additional plaintext characters. The message field explains what the linker error means. The "fix" field contains an action that a programmer could do to directly resolve the error. The JSON object: "#;
+
+    let content = format!(
+        r#"The following linker error: "{}" with the related symbols: "{}". {}"#,
+        linker_msg, sym_messages, post_prompt
+    );
+    error_fix(llm, content)
+}
+
+fn fix_compile_err(llm: &OpenAI, error: &Value) -> Result<Value, Box<dyn Error>> {
     let compiler_msg = error["message"].as_str().ok_or("message not found")?;
     let chunk = &error["context"];
 
     let post_prompt = r#"A fully JSON response with the schema: {"message": string, "fix": string} and no additional plaintext characters. The message field explains the error (in the context of the code). The "fix" field contains the full code chunk with updated changes, which ONLY fix the specified error. The JSON object: "#;
 
+    let content = format!(
+        r#"The code chunk: "{}" causes the error: "{}". {}"#,
+        chunk, compiler_msg, post_prompt
+    );
+
+    error_fix(llm, content)
+}
+
+fn error_fix(llm: &OpenAI, content: String) -> Result<Value, Box<dyn Error>> {
     let body = ChatBody {
         model: "model".into(),
         max_tokens: Some(99999),
@@ -23,10 +58,7 @@ pub fn error_fix(llm: &OpenAI, error: &Value) -> Result<Value, Box<dyn Error>> {
         messages: vec![Message {
             role: Role::User,
             // prompt only tested on OpenOrca models
-            content: format!(
-                r#"The code chunk: "{}" causes the error: "{}". {}"#,
-                chunk, compiler_msg, post_prompt
-            ),
+            content,
         }],
     };
 
@@ -42,6 +74,7 @@ pub fn error_fix(llm: &OpenAI, error: &Value) -> Result<Value, Box<dyn Error>> {
 }
 
 pub fn correct(input: Value) -> Value {
+    dbg!(&input);
     let base_url = match input["base_url"].as_str() {
         Some(url) => url,
         None => return json!({"result" : {"message" : "no base URL provided"}, "status" : 1}),
@@ -56,11 +89,16 @@ pub fn correct(input: Value) -> Value {
         }
     };
 
-    let fixes: Vec<Value> = compiler_errors
+    let linker_fixes = match fix_linker_err(&llm, &input["linker"]) {
+        Ok(fixes) => fixes,
+        Err(e) => return json!({"result" : {"message" : e.to_string()}, "status" : 1}),
+    };
+
+    let compiler_fixes: Vec<Value> = compiler_errors
         .iter()
         .flat_map(|f| f["errors"].as_array())
         .flatten()
-        .map(|e| error_fix(&llm, e))
+        .map(|e| fix_compile_err(&llm, e))
         .filter_map(|f| {
             if let Err(e) = f {
                 eprintln!("Parsing error: {}", e);
@@ -70,5 +108,6 @@ pub fn correct(input: Value) -> Value {
             }
         })
         .collect();
-    json!({"result" :{"fixes" : fixes}, "status" : 0})
+
+    json!({"result" :{"compiler_fixes" : compiler_fixes, "linker_fixes": linker_fixes}, "status" : 0})
 }
